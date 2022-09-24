a44b41cc4f847a48156264af51a6617c685cfcec
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2022-09-25 01:38:36 +0900

    시간 설정 창 제작 중 - 복약횟수(아침)제작중

--- noYakJa/app/src/main/java/com/cauiot/noyakja/MedicineSettingActivity.java
+++ noYakJa/app/src/main/java/com/cauiot/noyakja/MedicineSettingActivity.java
@@ -3,19 +3,44 @@ package com.cauiot.noyakja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
 
 import com.cauiot.noyakja.DB.Medicine;
+import com.cauiot.noyakja.databinding.ActivityMedicineSettingBinding;
 
 public class MedicineSettingActivity extends AppCompatActivity {
 
+    private final String TAG = "MedicineSettingActivity";
+
+    private ActivityMedicineSettingBinding activityMedicineSettingBinding;
+
     private Medicine medicine;
-    private
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medicine_setting);
+
+        activityMedicineSettingBinding = ActivityMedicineSettingBinding.inflate(getLayoutInflater());
+        View view = activityMedicineSettingBinding.getRoot();
+        setContentView(view);
+
+
+        activityMedicineSettingBinding.morningButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b == true) {
+                    activityMedicineSettingBinding.morningButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.medicine_count_background_on, getTheme()));
+                    activityMedicineSettingBinding.morningButton.setTextColor(getResources().getColor(R.color.white,getTheme()));
+
+                }else{
+                    activityMedicineSettingBinding.morningButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.medicine_count_background_off, getTheme()));
+                    activityMedicineSettingBinding.morningButton.setTextColor(getResources().getColor(R.color.blue,getTheme()));
+                }
+            }
+        });
     }
 
-
 }
+
